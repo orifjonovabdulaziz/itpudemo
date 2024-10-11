2e68b637804848e2ca4b0fb84b39d39a70ecc5b3
Abdulaziz Orifjonov <Abdulaziz_Orifjonov@student.itpu.uz>
2024-10-11 19:42:08 +0500

    set new data

--- my_python.py
+++ my_python.py
@@ -2,4 +2,4 @@ def self_presentation(*args) -> str:
     return f"Hi there my name is {args[0]}\nI'm {args[1]} years old."
 
 
-print(self_presentation('Abdulaziz',20))
+print(self_presentation('Abdulaziz',15))
